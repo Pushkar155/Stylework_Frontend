8dd6db21d158b16fd83b8dd5c9c8434f4c2ecf3b
Pushkar155 <96383324+Pushkar155@users.noreply.github.com>
2026-09-23 14:46:24 +0530

    fix backend url error

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL,
+  baseURL: "https://stylework-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
